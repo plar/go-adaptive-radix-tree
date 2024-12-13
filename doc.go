8b4a8069466080883f48df2f05dde63364fe4d1f
Pavel Larkin <laxkin@gmail.com>
2024-12-13 13:09:48 -0800

    Fix gofmt
    Signed-off-by: Pavel Larkin <laxkin@gmail.com>

--- doc.go
+++ doc.go
@@ -5,34 +5,34 @@
 //
 // Usage:
 //
-//     package main
+//	package main
 //
-//     import (
-//         "fmt"
+//	import (
+//	    "fmt"
 //
-//         art "github.com/plar/go-adaptive-radix-tree/v2"
-//     )
+//	    art "github.com/plar/go-adaptive-radix-tree/v2"
+//	)
 //
-//     func main() {
+//	func main() {
 //
-//         tree := art.New()
+//	    tree := art.New()
 //
-//         tree.Insert(art.Key("Hi, I'm Key"), "Nice to meet you, I'm Value")
-//         value, found := tree.Search(art.Key("Hi, I'm Key"))
-//         if found {
-//             fmt.Printf("Search value=%v\n", value)
-//         }
+//	    tree.Insert(art.Key("Hi, I'm Key"), "Nice to meet you, I'm Value")
+//	    value, found := tree.Search(art.Key("Hi, I'm Key"))
+//	    if found {
+//	        fmt.Printf("Search value=%v\n", value)
+//	    }
 //
-//         tree.ForEach(func(node art.Node) bool {
-//             fmt.Printf("Callback value=%v\n", node.Value())
-//             return true
-//         })
+//	    tree.ForEach(func(node art.Node) bool {
+//	        fmt.Printf("Callback value=%v\n", node.Value())
+//	        return true
+//	    })
 //
-//         for it := tree.Iterator(); it.HasNext(); {
-//             value, _ := it.Next()
-//             fmt.Printf("Iterator value=%v\n", value.Value())
-//         }
-//     }
+//	    for it := tree.Iterator(); it.HasNext(); {
+//	        value, _ := it.Next()
+//	        fmt.Printf("Iterator value=%v\n", value.Value())
+//	    }
+//	}
 //
 // Output:
 // Search value=Nice to meet you, I'm Value
